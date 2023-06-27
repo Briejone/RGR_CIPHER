49c31563a815b8f9cbb31e6f3c37dc755db2aa96
briejone <kirimonoo@mail.ru>
2023-06-27 17:54:39 +0700

    CaesarCipher.cc

---
+++ src/Ciphers/CaesarCipher.cc
@@ -0,0 +1,52 @@
+#include <iostream>  
+#include <fstream>  
+#include <algorithm>  
+#include <string>
+#include <cctype>
+#include <math.h>   
+using namespace std;
+
+string encryptCaesar(string message, int key) { //шифрование
+    string result = "";
+    for (int i = 0; i < message.length(); i++) {
+        if (isalpha(message[i])) { //проверка на букву алфавита
+            if (message[i] >= 'a' && message[i] <= 'z') { //для нижнего регистра
+                char ch = (message[i] + key - 'a') % 26 + 'a';
+                result += ch;
+            }
+            else { //для верхнего регистра
+                char ch = (message[i] + key - 'A') % 26 + 'A';
+                result += ch;
+            }
+        }
+        else { //обработка ошибки
+            cout << "Ошибка! Сообщение должно состоять только из букв английского алфавита." << endl;
+            exit(1); //прерывание программы при ошибке
+        }
+    }
+    return result;
+}
+
+
+string decryptCaesar(string message, int key) { //дешифрование
+    string result = "";
+    for (int i = 0; i < message.length(); i++) {
+        char ch = message[i];
+        if (isalpha(ch)) { //проверка на то, является ли буквой
+            if (islower(ch)) { //для нижнего регистра
+                ch -= key;
+                if (ch < 'a') {
+                    ch += 26;
+                }
+            }
+            else { //для верхнего регистра
+                ch -= key;
+                if (ch < 'A') {
+                    ch += 26;
+                }
+            }
+        }
+        result += ch;
+    }
+    return result;
+}
---
+++ src/Ciphers/CodewordCipher.cc
@@ -0,0 +1,93 @@
+#include <iostream>        
+#include <fstream>        
+#include <string>        
+#include <algorithm> // для функции replace    
+#include <cstring> 
+ 
+using namespace std; 
+ 
+const int ALPHABET_SIZE = 26; 
+ 
+ 
+void createNewAlphabet(char* newAlphabet) { 
+    char originalAlphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    char word[ALPHABET_SIZE + 1]; 
+    bool validInput = false; 
+    while (!validInput) { 
+        cout << "Введите ключ: "; 
+        cin >> word; 
+        validInput = true; 
+        int wordLength = strlen(word); 
+        for (int i = 0; i < wordLength; i++) { 
+            if (!isupper(word[i])) { // Проверяем, что символ является заглавной буквой 
+                validInput = false; 
+                break; 
+            } 
+        } 
+        if (!validInput) { 
+            std::cout << "Ошибка: введите только заглавные буквы английского алфавита" << std::endl; 
+        } 
+    } 
+ 
+    // Добавляем буквы из слова в новый алфавит 
+    int wordLength = strlen(word); 
+    for (int i = 0; i < wordLength; i++) { 
+        newAlphabet[i] = word[i]; 
+    } 
+ 
+    // Добавляем остальные буквы из исходного алфавита 
+    int j = wordLength; 
+    for (int i = 0; i < ALPHABET_SIZE; i++) { 
+        char letter = originalAlphabet[i]; 
+        if (strchr(word, letter) == nullptr) { // Проверяем, что буква не была добавлена ранее 
+            newAlphabet[j] = letter; 
+            j++; 
+        } 
+    } 
+    newAlphabet[ALPHABET_SIZE] = '\0'; 
+} 
+ 
+ 
+string encode_text(string source_text, char* ALPHABET_ENCODED) { 
+    string text_encoded; // строка для хранения закодированного текста 
+    string encoded_text; 
+    char ALPHABET_DECODED[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+     
+    for (auto c : source_text) { 
+        auto it = std::find(std::begin(ALPHABET_DECODED), std::end(ALPHABET_DECODED), std::toupper(c)); 
+        if (it != std::end(ALPHABET_DECODED)) { 
+            int index = std::distance(std::begin(ALPHABET_DECODED), it); 
+            text_encoded += std::toupper(ALPHABET_ENCODED[index]); 
+        } 
+        else { 
+            text_encoded += c; 
+        } 
+        encoded_text += text_encoded + "\n"; 
+         
+    } 
+    cout << "\nКодированный текст: \n\n" << text_encoded << std::endl; 
+    return text_encoded; 
+} 
+ 
+string decode_text(string encoded_text, char* ALPHABET_ENCODED) { 
+    string decoded_text; 
+    char ALPHABET_DECODED[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+ 
+    for (int i = 0; i < encoded_text.size(); i++) { 
+        char c = encoded_text[i]; 
+        bool found = false; 
+        for (int j = 0; j < ALPHABET_SIZE; j++) { 
+            if (toupper(c) == ALPHABET_ENCODED[j]) { 
+                decoded_text += toupper(ALPHABET_DECODED[j]); 
+                found = true; 
+                break; 
+            } 
+        } 
+        if (!found) { 
+            decoded_text += c; 
+        } 
+         
+    } 
+    cout << "\nДекодированный текст: \n\n" << decoded_text << std::endl; 
+    return decoded_text; 
+} 
